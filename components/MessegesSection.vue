e7ee618d5bbb695493d179761706be0364a106c0
Halla Mahyn <hallamahyn@gmail.com>
2021-03-02 18:10:51 +0300

    wip: messenger

---
+++ components/MessegesSection.vue
@@ -0,0 +1,36 @@
+<template>
+
+</template>
+<script>
+import MessengerIcon from "@/assets/icons/messengerIcon.svg"
+
+
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+
+  },
+  components: {
+
+  },
+
+  methods: {
+
+  },
+
+  mounted: function () {
+
+  }
+}
+
+</script>
+
+
+
+<style lang="scss">
+
+
+</style>
--- pages/messenger/_id.vue
+++ pages/messenger/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dialog-wrapper">
-    <div class="messages">
+    <div :class="{'messages': true, 'highlight': highlightFeed}" >
       <div class="messages-wrapper" v-for="(m, index) in messages"  :key="m.id + index"
         :style="{
           opacity: startAnimation ? '1' : '0',
@@ -13,6 +13,9 @@
           'messages-message': true,
           'messages-message_regular': m.type === 'regular',
           'messages-message_task': m.type === 'task',
+          'messages-message_system': m.type === 'system',
+          'messages-message_option': m.type === 'answer',
+          'messages-message_option--selected': m.id === answerSelected
         }"
       >
         <div class="messages-message-title" v-if="m.type === 'task'"> {{m.title}} </div>
@@ -22,14 +25,13 @@
         <div
         :class="{
           'messages-message_option': true,
-          'messages-message_option--selected': o.id === answerSelected
+          'messages-message_option--selected': o.id === answerSelected,
+          'messages-message_option--disabled': answerIds.includes(o.id)
         }"
-        v-if="m.type === 'task'"
-        v-for="(o, i) in m.options" 
-        :key="o.id + i" 
-        @click="setAnswerSelected(o.id)"
+        v-for="(o, i) in m.options"
+        :key="o.id + i"
+        @click="setAnswerSelected(o, m)"
         >
-          <div class="messages-message_option_order">{{i + 1}}</div>
           <div class="messages-message_option_text">{{o.text}}</div>
         </div>
       </div>
@@ -48,12 +50,17 @@
 
 <script>
 
+import sections  from "./data.js"
+
 export default {
 
   data() {
     return {
     startAnimation: false,
     answerSelected: null,
+    highlightFeed: false,
+    doFilter: true,
+    answerIds: [],
     chapters: [
       {id: 1, title: "first"},
       {id: 2, title: "two"},
@@ -64,6 +71,7 @@ export default {
       {id: 7, title: "3" },
       {id: 8, title: "4"}
     ],
+    currentSection: 0,
     characters: [
         {
           id: 1,
@@ -78,14 +86,15 @@ export default {
               id: 2,
               text: "finally got around to downloading asymmetry, huh",
               type: "regular",
-              withAnswer: true,
-              answer: {id: 1, text: "hey plato. thanks for the invite!", nextId: 3}
+              options: [
+                {
+                  id: 3,
+                  text: "hey plato. thanks for the invite!",
+                  type: "answer"
+                }
+              ]
             },
-            {
-              id: 3,
-              text: "no worries, i'm sure you'll help me later ;)",
-              type: "regular",
-            },
+
           ]
         },
         {
@@ -141,8 +150,31 @@ export default {
       }, 200)
     },
 
-    setAnswerSelected (value) {
-      this.answerSelected = value
+    setAnswerSelected (option, message) {
+      if (this.answerIds.includes(option.id)) return
+
+      let optionIds = message?.options.map(opt => opt.id)
+
+      let nextSection = sections[this.currentSection] || []
+
+      if (option?.highlight === false) {
+        this.highlightFeed = !this.highlightFeed
+      }
+
+
+      if (option?.messages?.length > 0) {
+        this.characters[$nuxt.$route.params.id - 1].messages.push(...option.messages)
+        this.characters[$nuxt.$route.params.id - 1].messages.push(...nextSection)
+      } else {
+        this.characters[$nuxt.$route.params.id - 1].messages.push(...nextSection)
+      }
+
+
+
+
+      this.answerSelected = option.id
+      this.answerIds.push(...optionIds)
+      this.currentSection++
     }
   },
 
@@ -155,6 +187,19 @@ export default {
   watch: {
     '$nuxt.$route.params.id': function(val) {
       this.startTimer()
+    },
+    'messages': function(messages) {
+      console.log("hui sosu", messages)
+      if (this.doFilter) {
+        let target = messages.find(el => el?.highlight === true)
+        if (target) {
+          this.highlightFeed = !this.highlightFeed
+          this.doFilter = false
+        }
+
+      }
+
+
     }
   }
 }
@@ -182,20 +227,20 @@ export default {
    padding-bottom: 42px;
 
   &-message {
-    /* max-width: 606px; */
-    width: 100%;
+    max-width: 606px;
     padding: 21px 24px;
     border-radius: 12px;
     margin-bottom: 12px;
+
   }
- 
+
 }
 
 .messages-wrapper {
-  width: 100%;
   display: flex;
+  width: 100%;
   flex-flow: column nowrap;
-  align-items: flex-end;
+  align-items: flex-start;
   justify-content: flex-start;
 
   opacity: 0;
@@ -210,7 +255,15 @@ export default {
 
 .messages-message_regular {
   background: #F3F3F3;
+}
 
+.messages-message_answer {
+  align-self: flex-end;
+}
+.messages-message_system {
+  align-self: center;
+  max-width: 600px;
+  font-size: 24px;
 }
 
 .messages-message_task {
@@ -243,6 +296,12 @@ export default {
   padding: 18px;
   justify-content: flex-start;
   cursor: pointer;
+  &--disabled {
+    user-select: none;
+    opacity: 0.7;
+    cursor: default;
+  }
+
 }
 
 .messages-message_option_order {
@@ -261,6 +320,7 @@ export default {
   border-radius: 12px;
   margin-right: 18px;
 }
+
 
 
 .messages-message_option--selected {
@@ -367,6 +427,9 @@ export default {
           inset -2px -2px 4px rgba(255, 255, 255, 0.9),
            inset 2px 2px 5px rgba(207, 207, 207, 0.9);
   }
+}
+.highlight {
+  /* border: 5px solid blue */
 }
 
 
