26c2c6f2a2a0927db3e1bc655a47194e97b58716
Halla Mahyn <hallamahyn@gmail.com>
2021-05-09 15:36:42 +0300

    fix forgot password validations

--- pages/forgot_password.vue
+++ pages/forgot_password.vue
@@ -39,7 +39,7 @@
         <div class="forgot-password-container_button">
           <Button
             title="Next"
-            :isBtnActive="password.length >= 8 && password === passwordConfirm"
+            :isBtnActive="validationMessages.every(el => el === null)"
             :isLoading="isLoading"
             @btnClick="changePassword"
           />
